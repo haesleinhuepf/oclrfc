778a3c387644b480ca80c12bd73899927f2d23e2
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-08-15 20:59:57 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="oclrfc",
-    version="0.4.0",
+    version="0.4.1",
     author="haesleinhuepf",
     author_email="robert.haase@tu-dresden.de",
     description="OpenCL-based Random Forest Classifier",
